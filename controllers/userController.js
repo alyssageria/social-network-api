5f7b49770c2a72573aa18550999dcca57c4fc89e
Alyssa Geria <alyssageria@gmail.com>
2023-05-24 13:40:25 -0700

    file structure for routes/controllers

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    //get all users
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    }
+}
