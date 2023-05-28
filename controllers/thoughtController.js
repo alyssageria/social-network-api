0040abee6d083e7db0aba7bb6aa40f6ed3d41ad2
Alyssa Geria <alyssageria@gmail.com>
2023-05-28 13:53:43 -0700

    reaction routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -66,4 +66,34 @@ module.exports = {
             .then(() => res.json({ message: 'Thought deleted!' }))
             .catch((err) => res.status(500).json(err));
     },
+
+    // POST - add reaction
+    addReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // DELETE - delete reaction
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "No thought find with this ID!" })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,14 +4,17 @@ const { getOneThought,
     getThoughts,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+    deleteReaction
 } = require('../../controllers/thoughtController');
 
 
 router.route('/').get(getThoughts).post(createThought);
 
 router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
-
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 
 module.exports = router;
